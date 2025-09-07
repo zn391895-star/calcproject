bfda4d54b19f1191bfc0aa2103bde7df837e6d80
zn391895-star <zn391895@gmail.com>
2025-09-07 19:30:47 +0500

    First commit

--- calcproject/settings.py
+++ calcproject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 # STATICFILES_DIRS = [ BASE_DIR / "static" ]   # apna static folder ka path
 # STATIC_ROOT = BASE_DIR / "staticfiles"       # production me collectstatic ke liye
 
